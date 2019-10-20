191c7bc32d7f526242f0b33c373a61630fd8c4e2
Douglas Kincaid <douglas@registrationmax.com>
2019-10-20 19:55:25 -0400

    link list updates

--- list/single-linked-list.js
+++ list/single-linked-list.js
@@ -6,14 +6,37 @@ class Node {
     }
 }
 
-export default class LinkedList {
+class LinkedList {
     constructor() {
         this.head = new Node(null);
     }
 
-    insert(value) {
+    push_front(value) {
         const newNode = new Node(value);
-        const current = this.head;
+
+        if(this.head.next === null) {
+            this.head.next = newNode;
+        } else {
+            newNode.next = this.head.next;
+            this.head.next = newNode;
+        }
+    }
+
+    pop_front() {
+        let node;
+
+        if(this.head.next === null) {
+            return null;
+        } else {
+            node = this.head.next;
+            this.head.next = node.next;
+            return node.value;
+        }
+    }
+
+    push_back(value) {
+        const newNode = new Node(value);
+        let current = this.head;
 
         while(current.next !== null) {
             current = current.next;
@@ -21,4 +44,108 @@ export default class LinkedList {
 
         current.next = newNode;
     }
+
+    pop_back() {
+        let current = this.head;
+        let previous;
+
+        while(current.next !== null) {
+            previous = current;
+            current = current.next;
+        }
+
+        previous.next = null;
+
+        return current.value;
+    }
+
+    size() {
+        let count = 0;
+        let current = this.head;
+
+        while(current.next !== null) {
+            current = current.next;
+            count++;
+        }
+
+        return count;
+    }
+
+    empty() {
+        return this.head.next === null;
+    }
+
+    value_at(index) {
+        let current = this.head;
+        let count = -1;
+
+        while(current.next !== null && count !== index) {
+            current = current.next;
+            count++;
+        }
+
+        if(count === index) {
+            return current.value;
+        } else {
+            return null;
+        }
+    }
+
+    front() {
+        let node;
+
+        if(this.head.next === null) {
+            return null;
+        } else {
+            node = this.head.next;
+            return node.value;
+        }
+    }
+
+    back() {
+        let current = this.head;
+
+        while(current.next !== null) {
+            current = current.next;
+        }
+
+        return current.value;
+    }
+
+    insert_at(index, value) {
+        let newNode = new Node(value);
+        let current = this.head;
+        let count = -1;
+
+        while(current.next !== null && count !== index) {
+            current = current.next;
+            count++;
+        }
+        
+        if(count === index) {
+            if(current.next === null) {
+                current.next = newNode;
+            } else {
+                newNode = current.next;
+                current.next = newNode;
+            }
+        }
+    }
+
+    erase_at(index) {
+        let current = this.head;
+        let previous;
+
+        let count = -1;
+
+        while(current.next !== null && count !== index) {
+            previous = current;
+            current = current.next;
+            count++;
+        }
+
+        previous.next = current.next;
+    }
 }
+
+module.exports = LinkedList;
--- list/single-linked-list.main.js
+++ list/single-linked-list.main.js
@@ -1,11 +1,14 @@
-import LinkedList from './single-linked-list';
+const LinkedList = require('./single-linked-list');
 
 function main() {
     const linkedList = new LinkedList();
 
-    linkedList.insert(3);
+    linkedList.push_front(3);
+    linkedList.push_front(4);
 
-    console.log(linkedList);
+
+    console.log(linkedList.pop_back());
+    console.log(linkedList.size());
 }
 
 main();
