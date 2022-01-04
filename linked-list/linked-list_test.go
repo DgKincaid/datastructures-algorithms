a14d08b0d832895e5147bfc17188aabe97fd525d
Douglas Kincaid <DgKincaid@users.noreply.github.com>
2022-01-04 08:23:20 -0500

    todays notes

--- linked-list/linked-list_test.go
+++ linked-list/linked-list_test.go
@@ -8,16 +8,16 @@ import (
 
 func TestLinkedList(t *testing.T) {
 
-	linkedList := NewLinkedList("test value")
+	linkedList := NewLinkedList("head")
 
-	linkedList.Add("another value 1")
-	linkedList.Add("another value 2")
-	linkedList.Add("another value 3")
-	linkedList.Add("another value 4")
+	linkedList.Add("value 1")
+	linkedList.Add("value 2")
+	linkedList.Add("value 3")
+	linkedList.Add("value 4")
 
-	// fmt.Print(linkedList)
-
-	// linkedList.Print()
-
-	assert.True(t, false)
+	assert.Equal(t, "head", linkedList.Head.Value)
+	assert.Equal(t, "value 1", linkedList.Head.Next.Value)
+	assert.Equal(t, "value 2", linkedList.Head.Next.Next.Value)
+	assert.Equal(t, "value 3", linkedList.Head.Next.Next.Next.Value)
+	assert.Equal(t, "value 4", linkedList.Head.Next.Next.Next.Next.Value)
 }
